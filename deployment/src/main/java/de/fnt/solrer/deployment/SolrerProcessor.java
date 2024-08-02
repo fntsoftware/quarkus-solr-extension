ad4e41dba641f85f118c0ddd15adb83d445003c2
Johannes Gerold <johannes.gerold@fntsoftware.com>
2024-08-02 11:42:41 +0200

    initial snapshot

---
+++ deployment/src/main/java/de/fnt/solrer/deployment/SolrerProcessor.java
@@ -0,0 +1,73 @@
+package de.fnt.solr.deployment;
+
+import de.fnt.solr.runtime.SolrClientProducer;
+import de.fnt.solr.runtime.SolrerDevserviceConfig;
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.deployment.IsNormal;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
+import io.quarkus.deployment.builditem.LaunchModeBuildItem;
+import io.quarkus.runtime.LaunchMode;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.images.builder.dockerfile.statement.MultiArgsStatement;
+
+import java.util.Map;
+import java.util.function.BooleanSupplier;
+
+class SolrerProcessor {
+    SolrerDevserviceConfig config;
+
+    private static final String FEATURE = "solr";
+
+    @BuildStep
+    public AdditionalBeanBuildItem producer() {
+        return new AdditionalBeanBuildItem(SolrClientProducer.class);
+    }
+
+    @BuildStep(onlyIfNot = IsNormal.class, onlyIf = WantsSolrDevService.class)
+    public DevServicesResultBuildItem createContainer(LaunchModeBuildItem launchMode) {
+        ImageFromDockerfile image = new ImageFromDockerfile().withFileFromClasspath(".", "solr")
+                .withDockerfileFromBuilder(builder -> {
+                    builder.from("solr:" + config.version()).withStatement(
+                            new MultiArgsStatement("COPY --chown=solr:solr", ".", "/var/solr/data/" + config.core()));
+                });
+        SolrContainer container = new SolrContainer(image);
+        container.start();
+        Map<String, String> props = Map.of("quarkus.solr.url", "http://" + container.getHost() + ":"
+                + container.getMappedPort(container.getPort()) + "/solr/" + config.core());
+        return new DevServicesResultBuildItem.RunningDevService(FEATURE, container.getContainerId(), container::close,
+                props).toBuildItem();
+    }
+
+    static class WantsSolrDevService implements BooleanSupplier {
+        LaunchMode launchMode;
+        SolrerDevserviceConfig config;
+
+        public boolean getAsBoolean() {
+            Boolean devServicesActive = ConfigProvider.getConfig().getValue("quarkus.devservices.enabled",
+                    Boolean.class);
+            return launchMode == LaunchMode.DEVELOPMENT && devServicesActive && config.enabled();
+        }
+    }
+
+    private static class SolrContainer extends GenericContainer<SolrContainer> {
+        static final int PORT = 8983;
+
+        public SolrContainer(ImageFromDockerfile image) {
+            super(image);
+        }
+
+        public int getPort() {
+            return PORT;
+        }
+
+        @Override
+        protected void configure() {
+            addExposedPort(PORT);
+            waitingFor(Wait.forLogMessage(".*Started Server.*", 1));
+        }
+    }
+}
---
+++ runtime/src/main/java/de/fnt/solrer/runtime/SolrerDevserviceConfig.java
@@ -0,0 +1,33 @@
+package de.fnt.solr.runtime;
+
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+
+@ConfigMapping(prefix = "quarkus.solr.devservices")
+@ConfigRoot(phase = ConfigPhase.BUILD_TIME)
+public interface SolrerDevserviceConfig {
+    /**
+     * Enable the Solr dev service
+     *
+     * @return
+     */
+    @WithDefault("true")
+    boolean enabled();
+
+    /**
+     * Which Core the devservice should create
+     *
+     * @return
+     */
+    String core();
+
+    /**
+     * Which Solr version to use
+     *
+     * @return
+     */
+    @WithDefault("9.6.1")
+    String version();
+}
