d9f9aa46d91f79242ee6ed7295cec1f99b6ba11d
Johannes Gerold <johannes.gerold@fntsoftware.com>
2024-08-16 12:02:04 +0200

    fix(devservice): stop contianer when devservice is stopped

--- deployment/src/main/java/de/fnt/solr/deployment/SolrProcessor.java
+++ deployment/src/main/java/de/fnt/solr/deployment/SolrProcessor.java
@@ -5,20 +5,21 @@ import de.fnt.solr.runtime.SolrDevserviceConfig;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.IsNormal;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CuratedApplicationShutdownBuildItem;
 import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
-import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.runtime.LaunchMode;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.images.builder.dockerfile.statement.MultiArgsStatement;
-
+import java.io.IOException;
 import java.util.Map;
 import java.util.function.BooleanSupplier;
 
 class SolrProcessor {
     SolrDevserviceConfig config;
+    static volatile DevServicesResultBuildItem.RunningDevService devService;
 
     private static final String FEATURE = "solr";
 
@@ -28,9 +29,23 @@ class SolrProcessor {
     }
 
     @BuildStep(onlyIfNot = IsNormal.class, onlyIf = WantsSolrDevService.class)
-    public DevServicesResultBuildItem createContainer(LaunchModeBuildItem launchMode) {
-        ImageFromDockerfile image = new ImageFromDockerfile().withFileFromClasspath(".", "solr")
-                .withDockerfileFromBuilder(builder -> {
+    public DevServicesResultBuildItem createContainer(CuratedApplicationShutdownBuildItem closeBuildItem) {
+        if (devService != null) {
+            return null;
+        }
+        Runnable closeTask = () -> {
+            if (devService != null) {
+                try {
+                    devService.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            devService = null;
+        };
+        closeBuildItem.addCloseTask(closeTask, false);
+        ImageFromDockerfile image = new ImageFromDockerfile("quarkus/devservices/solr")
+                .withFileFromClasspath(".", "solr").withDockerfileFromBuilder(builder -> {
                     builder.from("solr:" + config.version()).withStatement(
                             new MultiArgsStatement("COPY --chown=solr:solr", ".", "/var/solr/data/" + config.core()));
                 });
@@ -38,8 +53,9 @@ class SolrProcessor {
         container.start();
         Map<String, String> props = Map.of("quarkus.solr.url", "http://" + container.getHost() + ":"
                 + container.getMappedPort(container.getPort()) + "/solr/" + config.core());
-        return new DevServicesResultBuildItem.RunningDevService(FEATURE, container.getContainerId(), container::close,
-                props).toBuildItem();
+        devService = new DevServicesResultBuildItem.RunningDevService(FEATURE, container.getContainerId(),
+                container::close, props);
+        return devService.toBuildItem();
     }
 
     static class WantsSolrDevService implements BooleanSupplier {
@@ -66,6 +82,7 @@ class SolrProcessor {
 
         @Override
         protected void configure() {
+            super.configure();
             addExposedPort(PORT);
             waitingFor(Wait.forLogMessage(".*Started Server.*", 1));
         }
